f7e3438a2616e97b56181279bde72135e01393dd
Yanqing Wu <y433wu@edu.uwaterloo.ca>
2023-03-23 03:18:40 -0600

    DEV: support keyboard while selecting moves #9

--- include/gui/popup_selection.h
+++ include/gui/popup_selection.h
@@ -2,10 +2,8 @@
  * @author      Yanqing Wu
  * @email       meet.yanqing.wu@gmail.com
  * @create date 2023-02-10 05:27:59
- * @modify date 2023-02-11 11:49:04
- * @desc Popup selection for moves.
- *
- * TODO: keyboard input
+ * @modify date 2023-03-23 02:56:41
+ * @desc Popup selection for moves. Support keyboard input.
  */
 #ifndef FG_GUI_POPUP_SELECTION_H_
 #define FG_GUI_POPUP_SELECTION_H_
@@ -15,6 +13,7 @@
 #include <vector>
 // Qt
 #include <QGridLayout>
+#include <QKeyEvent>
 #include <QMainWindow>
 
 namespace gui {
@@ -36,10 +35,14 @@ class PopupSelection : public QMainWindow {
   static std::mutex mutex_;
   /////////////////////////////////////
  protected:
+  std::vector<uint8_t> moves_;
+
   QGridLayout *layout_;
   QWidget *widget_;
 
-  void initUI(std::vector<uint8_t> available_moves);
+  void initUI();
+  void keyPressEvent(QKeyEvent *event) override;
+  bool isValidKey(uint8_t key);
  signals:
   void selectedNumber(QString s);
 };
