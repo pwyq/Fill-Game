db37d3266d5ce9e843475379a419a93abbe89d30
Yanqing Wu <y433wu@edu.uwaterloo.ca>
2023-03-31 19:28:08 -0600

    FIX windows build

--- src/gui/helper.cpp
+++ src/gui/helper.cpp
@@ -4,14 +4,15 @@
  * @create date 2023-02-10 05:31:58
  * @modify date 2023-03-31 16:38:46
  */
-// local
+
 #include "gui/helper.h"
 
 namespace gui::helper {
 
 bool randomBool() {
-  static auto gen = std::bind(std::uniform_int_distribution<>(0, 1), std::default_random_engine());
-  return gen();
+  static std::mt19937_64 gen(std::random_device{}());
+  static std::uniform_int_distribution<int> dis(0, 1);
+  return dis(gen) == 1;
 }
 
 }  // namespace gui::helper
